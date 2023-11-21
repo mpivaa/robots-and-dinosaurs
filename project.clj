03af1eeb45a2166a60afcc7996a442827c7d6e5b
Marcelo Piva <marcelo.piva@nubank.com.br>
2023-11-21 17:17:12 -0300

    update clojure

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Robots vs Dinosaurs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.match "0.3.0-alpha5"]
                  [clojure-lanterna "0.9.7"]
                  [clj-fuzzy "0.4.1"]
